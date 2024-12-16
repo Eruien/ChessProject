eed45c9b5bb8f3c156f1a3dddf55fd2d50a16703
Eruien <87483479+Eruien@users.noreply.github.com>
2024-12-16 16:49:54 +0900

    임시 저장

---
+++ Assets/Scripts/Monster/Specter.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+public class Specter : BaseMonster
+{
+    protected new void Awake()
+    {
+        base.Awake();
+    }
+
+    protected new void OnEnable()
+    {
+        base.OnEnable();
+    }
+
+    protected new void Start()
+    {
+        base.Start();
+    }
+
+    protected new void Update()
+    {
+        base.Update();
+    }
+
+    protected new void OnDisable()
+    {
+        base.OnDisable();
+    }
+
+    protected override void SetBlackBoardKey()
+    {
+        target = targetLabo;
+        blackBoard.m_TargetObject.Key = target;
+        blackBoard.m_HP.Key = Managers.Data.monsterDict[this.GetType().Name].hp;
+        blackBoard.m_AttackDistance.Key = Managers.Data.monsterDict[this.GetType().Name].attackDistance;
+        blackBoard.m_AttackRange.Key = Managers.Data.monsterDict[this.GetType().Name].attackRange;
+        blackBoard.m_AttackRangeCorrectionValue.Key = Managers.Data.monsterDict[this.GetType().Name].attackRangeCorrectionValue;
+        blackBoard.m_DefaultAttackDamage.Key = Managers.Data.monsterDict[this.GetType().Name].defaultAttackDamage;
+        blackBoard.m_MoveSpeed.Key = Managers.Data.monsterDict[this.GetType().Name].moveSpeed;
+        blackBoard.m_ProjectTileSpeed.Key = Managers.Data.monsterDict[this.GetType().Name].projectTileSpeed;
+    }
+
+    protected override void ChildAttack() { }
+
+    protected override void OnChildHitEvent() { }
+}
