d12a39824c076c5001aee5b67c6c49814578f138
Eruien <87483479+Eruien@users.noreply.github.com>
2025-01-18 20:41:20 +0900

    저장용

--- Assets/Scripts/TestScripts.cs
+++ Assets/Scripts/TestScripts.cs
@@ -1,20 +1,11 @@
-using Assets.Scripts;
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class TestScripts : MonoBehaviour
 {
-    
-
-    [SerializeField]
-    public GameObject testObj;
-    
     // Start is called before the first frame update
     void Start()
     {
-        
+        Vector3 vec = GameObject.Find("RedTeamSpawnSpot").GetComponent<MeshCollider>().bounds.size;
     }
 
     // Update is called once per frame
